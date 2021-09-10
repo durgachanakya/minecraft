7d27dac2a996f4cf7b18aa4b2cacff6eb02f58c3
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2021-09-10 17:40:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -122,3 +122,43 @@ function my_keydown(e){
         console.log("c");
     }
 }
+
+function up(){
+    if(player_y >=0){
+        player_y = player_y - block_img_height;
+        console.log("block image height = " + block_img_height);
+        console.log("when up arrow key is pressed , X = " + player_x + ",Y = " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down(){
+    if(player_y <=500){
+        player_y = player_y + block_img_height;
+        console.log("block image height = " + block_img_height);
+        console.log("when down arrow key is pressed , X = " + player_x + ",Y = " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function left(){
+    if(player_x >=0){
+        player_x = player_x - block_img_width;
+        console.log("block image width = " + block_img_width);
+        console.log("when left arrow key is pressed , X = " + player_x + ",Y = " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function right(){
+    if(player_x <=850){
+        player_x = player_x + block_img_width;
+        console.log("block image width = " + block_img_width);
+        console.log("when right arrow key is pressed , X = " + player_x + ",Y = " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
